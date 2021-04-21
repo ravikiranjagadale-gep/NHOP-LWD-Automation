722bb462ccaf1c28f64b6a52cb5cb97e23e3efbe
AllenRichy-Gep <allen_richy@gep.com>
2021-04-21 12:37:39 +0000

    Allen

---
+++ Checkbox.js
@@ -0,0 +1,7 @@
+ function uncheck(){
+	var inputs = document.querySelector("input[type='checkbox']");
+		for(var i = 0; i < inputs.length; i++) {
+			inputs[i].checked = true;   
+		}
+	return input	
+ }		
